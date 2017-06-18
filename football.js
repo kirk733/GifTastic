0e5fb26910002dba0cb91a2babee15b6b2676e68
Kirk Henderson <kirkhenderson@Kirks-iMac.local>
2017-06-18 15:00:20 -0600

    pausing gifs working

--- football.js
+++ football.js
@@ -1,4 +1,7 @@
-  // Adding click event listen listener to all buttons
+$(document).ready(function() {
+    var still = [];
+
+    // Adding click event listen listener to all buttons
     $("button").on("click", function() {
       // Grabbing and storing the data-football property value from the button
       var football = $(this).attr("data-football");
@@ -14,21 +17,52 @@
         .done(function(response) {
           // storing the data from the AJAX request in the results variable
           var results = response.data;
+
+          console.log(results);
+
           // Looping through each result item
           for (var i = 0; i < results.length; i++) {
             // Creating and storing a div tag
             var footballDiv = $("<div>");
+            footballDiv.addClass("gifFootball");
             // Creating a paragraph tag with the result item's rating
+
+
             var p = $("<p>").text("Rating: " + results[i].rating);
             // Creating and storing an image tag
             var footballImage = $("<img>");
-            // Setting the src attribute of the image to a property pulled off the result item
-            footballImage.attr("src", results[i].images.fixed_height.url);
-            // Appending the paragraph and image tag to the animalDiv
+
+      // Setting the src attribute of the still image to a property pulled off the result item
+            footballImage.attr("src", results[i].images.fixed_height_still.url);
+            footballImage.attr("data-still", results[i].images.fixed_height_still.url);
+            footballImage.attr("data-animate", results[i].images.fixed_height.url);
+            footballImage.attr("data-state", "still");
+            footballImage.addClass("moveornot");
+
+         
+            // Appending the paragraph and image tag to the footballDiv
             footballDiv.append(p);
             footballDiv.append(footballImage);
-            // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
+            // Prependng the footballDiv to the HTML page in the "#gifs-appear-here" div
             $("#gifs-appear-here").prepend(footballDiv);
           }
         });
     });
+
+   $(document).on("click", ".moveornot", function(){
+
+              // storing the data-state of the gif into a variable
+              var state = $(this).attr("data-state");
+
+          
+          if(state === "still"){
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+
+          } else{
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+          }
+ })
+
+  })
